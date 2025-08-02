4558b8d5cb2052d4eb1ef7778a94614b2e6fd521
ivasx <ambroziak.v.ivan@gmail.com>
2025-08-02 21:30:07 +0300

    update template for view tasks

--- tickify/tasks/views.py
+++ tickify/tasks/views.py
@@ -2,6 +2,12 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
+tasks_db = [
+    {'id': 1, 'title': 'Завдання 1', 'description': 'Опис завдання 1', 'is_done': False},
+    {'id': 2, 'title': 'Завдання 2', 'description': 'Опис завдання 2', 'is_done': True},
+    {'id': 3, 'title': 'Завдання 3', 'description': 'Опис завдання 3', 'is_done': False},
+    {'id': 4, 'title': 'Завдання 4', 'description': 'Опис завдання 4', 'is_done': True},
+]
 
 def home(request):
     data = {
@@ -12,7 +18,11 @@ def home(request):
     return render(request, "tasks/home.html", context=data)
 
 def tasks_list(request):
-    return render(request, "tasks/tasks_list.html", {'title': 'Tasks'})
+    data = {
+        'title': 'Tasks',
+        'tasks': tasks_db,
+    }
+    return render(request, "tasks/tasks_list.html", context=data)
 
 def tasks_detail(request, task_id):
     return HttpResponse(f"Детальна сторінка задачи з id = {task_id}.")
