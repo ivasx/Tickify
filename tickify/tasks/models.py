4032a1486bc2186902cfa769e5cf6f88ade10d57
ivasx <ambroziak.v.ivan@gmail.com>
2025-08-08 22:16:54 +0300

    create User model

--- tickify/tasks/models.py
+++ tickify/tasks/models.py
@@ -6,3 +6,8 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
+
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    
