05163f34790f9cc37f00c206db8118ccd13419e6
ivasx <ambroziak.v.ivan@gmail.com>
2025-08-18 22:13:31 +0300

    Task model update

--- tickify/tasks/models.py
+++ tickify/tasks/models.py
@@ -1,11 +1,34 @@
 from django.db import models
+from tickify import settings
+
 
 # Create your models here.
 
+class Priority(models.IntegerChoices):
+    DEFAULT = 0, "Default"
+    LOW = 1, "Low"
+    MEDIUM = 2, "Medium"
+    HIGH = 3, "High"
+    CRITICAL = 4, "Critical"
+
 class Task(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
+    priority = models.IntegerField(choices=Priority.choices, default=Priority.DEFAULT)
+    deadline = models.DateTimeField(blank=True, null=True)
+    category = models.CharField(max_length=255, blank=True, null=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    completed_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.title}"
 
 class User(models.Model):
     username = models.CharField(max_length=200)
---
+++ tickify/tasks/templatetags/task_tags.py
@@ -0,0 +1,14 @@
+from django import template
+import tasks.views as views
+
+register = template.Library()
+
+@register.simple_tag
+def get_task_list():
+    return views.tasks_db
+
+
+@register.inclusion_tag('tasks/tasklist.html')
+def show_tasks():
+    tasks = views.tasks_db
+    return {'tasks': tasks}
