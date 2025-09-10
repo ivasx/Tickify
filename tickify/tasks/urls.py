03d04a2260923e565b4db1a3f92891a78612264d
ivasx <ambroziak.v.ivan@gmail.com>
2025-09-10 21:45:06 +0300

    Fix category dropdown and category urls

--- tickify/tasks/urls.py
+++ tickify/tasks/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('tasks/', views.tasks_list, name='tasks_list'),
+    path('tasks/category/<slug:category_slug>/', views.tasks_list, name='category'),
     path('tasks/<slug:task_slug>/', views.tasks_detail, name='tasks_detail'),
-path('tasks/category/<slug:category_slug>/', views.show_category, name='category'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('contact/', views.contact, name='contact'),
