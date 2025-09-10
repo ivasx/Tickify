4e8bd5afa885162643b0fb9ba3da99da30b26237
ivasx <ambroziak.v.ivan@gmail.com>
2025-09-10 21:42:32 +0300

    Unify tasks views and fix category dropdown

--- tickify/tasks/views.py
+++ tickify/tasks/views.py
@@ -11,7 +11,8 @@ menu = [{'title': 'Home', 'url': 'home'},
         {'title': 'Contact', 'url': 'contact'},
         {'title': 'Login', 'url': 'login'},
         {'title': 'Register', 'url': 'register'}
-]
+        ]
+
 
 def register(request):
     if request.method == 'POST':
@@ -24,12 +25,14 @@ def register(request):
             user = authenticate(username=username, password=password)
             return redirect('home')
 
+
 def login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             user = form.save()
             return redirect('home')
+
 
 def home(request):
     categories = Category.objects.filter(user=request.user)
@@ -41,50 +44,31 @@ def home(request):
 
     return render(request, "tasks/home.html", context=data)
 
-def tasks_list(request):
+
+def tasks_list(request, category_slug=None):
     if not request.user.is_authenticated:
         return redirect('login')
+
+    categories = Category.objects.filter(user=request.user)
+    current_category = None
+
+    if category_slug:
+        current_category = get_object_or_404(Category, slug=category_slug, user=request.user)
+        completed_tasks = Task.completed_obj.filter(user=request.user, category=current_category)
+        uncompleted_tasks = Task.uncompleted_obj.filter(user=request.user, category=current_category)
     else:
         completed_tasks = Task.completed_obj.filter(user=request.user)
         uncompleted_tasks = Task.uncompleted_obj.filter(user=request.user)
-        categories = Category.objects.filter(user=request.user)
-        data = {
-            'title': 'Tasks',
-            'completed_tasks': completed_tasks,
-            'uncompleted_tasks': uncompleted_tasks,
-            'categories': categories,
-        }
-        return render(request, "tasks/tasks_list.html", context=data)
-
-def tasks_detail(request, task_slug):
-    task = get_object_or_404(Task,slug=task_slug)
 
     data = {
-        'title': task.title,
-        'task': task,
+        'title': 'Tasks',
+        'completed_tasks': completed_tasks,
+        'uncompleted_tasks': uncompleted_tasks,
+        'categories': categories,
+        'current_category': current_category,
     }
 
-    return render(request, "tasks/task_detail.html", context=data)
-
-def page_not_found(request, exception):
-    return HttpResponseNotFound("<h1>Сторінка не знайдена.</h1>")
-
-def contact(request):
-    return render(request, "tasks/contacts.html")
-
-def add_task(request):
-    if request.method == 'POST':
-        form = AddTaskForm(request.POST)
-        if form.is_valid():
-            print(form.cleaned_data)
-    else:
-        form = AddTaskForm()
-
-    data = {
-        'title': 'Add Task',
-        'form': form,
-    }
-    return render(request, "tasks/add_task.html", context=data)
+    return render(request, "tasks/tasks_list.html", context=data)
 
 
 def show_category(request, category_slug):
@@ -100,3 +84,41 @@ def show_category(request, category_slug):
         'current_category': category,
     }
     return render(request, "tasks/tasks_list.html", context=data)
+
+
+def tasks_detail(request, task_slug):
+    task = get_object_or_404(Task, slug=task_slug)
+
+    data = {
+        'title': task.title,
+        'task': task,
+    }
+
+    return render(request, "tasks/task_detail.html", context=data)
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Сторінка не знайдена.</h1>")
+
+
+def contact(request):
+    return render(request, "tasks/contacts.html")
+
+
+def add_task(request):
+    if request.method == 'POST':
+        form = AddTaskForm(request.POST)
+        if form.is_valid():
+            try:
+                Task.objects.create(**form.cleaned_data, user=request.user)
+                return redirect('tasks_list')
+            except:
+                form.add_error(None, "Не вдалось додати задачу")
+    else:
+        form = AddTaskForm()
+
+    data = {
+        'title': 'Add Task',
+        'form': form,
+    }
+    return render(request, "tasks/add_task.html", context=data)
